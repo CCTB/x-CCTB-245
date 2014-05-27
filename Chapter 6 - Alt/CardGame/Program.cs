c55ce683b73c4f52af32a7f35e59a47873920dc4
Dan Gilleland <dgilleland@nait.ca>
2014-05-27 16:18:30 -0600

    TODO comments for next class

--- Chapter 6 - Alt/CardGame/Program.cs
+++ Chapter 6 - Alt/CardGame/Program.cs
@@ -13,3 +13,42 @@ namespace CardGame
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// LOGs....
+// At the end of this topic, you should be able to
+// TODO: 1) List the different kinds of collections we can use in C# 4.0 or higher
+// NOTE: 1a) http://stackoverflow.com/questions/247621/what-are-the-correct-version-numbers-for-c
+// TODO: 2) Identify which kind of collection type is supported directly by the C# language
+// TODO: 3) Compare and contrast the main characteristics of Arrays, Lists, Queues, Stacks, and Dictionaries
+// TODO: ) Create samples that illustrate the various kinds of collections
+// TODO: ) Explain what is meant by the term "Generics" and how it applies to the various collection classes
+
+// ) Stack - deck of cards (pop from top of stack, discard pile is empty stack that we push from)
+// ) Shuffle - take a deck of cards and shuffle them
+// ) Sort - take a deck of cards and sort them, first by suit, then by card face -- a) OrderBy extension method, b) LINQ to Objects query
+// ) Array -
+// ) List - 
+// ) Dictionary -
+
+
+// ) Abstract Classes
+// ) Interfaces
+// ) Extension Methods
